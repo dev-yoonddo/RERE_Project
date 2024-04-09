7453c9ce4ce6f70680838aef7dccadf14923aed4
yoonddo <oooo_0720@naver.com>
2024-04-09 19:08:52 +0900

    Creating and Using Custom Annotation

---
+++ test/src/main/java/com/login/demo/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.login.demo.config;
+
+import com.login.demo.oauth2.LoginUserArgumentResolver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final LoginUserArgumentResolver loginUserArgumentResolver;
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+        argumentResolvers.add(loginUserArgumentResolver);
+    }
+
+}
